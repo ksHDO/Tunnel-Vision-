ebe045fa9b3676d83bb6efcf94b91d670e841699
ksHDO <hiddendeathorb@gmail.com>
2018-02-15 07:46:20 -0700

    PlayerController.cs - Camera now zooms out after waiting for a while. You must stop firing for some time before the camera starts zooming out game.unity - Adjusted zoom values

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,7 +14,9 @@ public class PlayerController : MonoBehaviour
     [Header("Camera")]
     [SerializeField] private float _zoomMax = 2f;
     [SerializeField] private float _zoomMin = 6f;
-    [SerializeField] private float _zoomModifier = 0.01f;
+    [SerializeField] private float _timeToZoomOut = 1f;
+    [SerializeField] private float _zoomInSpeed = 0.01f;
+    [SerializeField] private float _zoomOutSpeed = 0.01f;
     [SerializeField] private float _smoothTime = 1f;
     [SerializeField] private float _maxSpeed = 1f;
     private float _zoomValue;
@@ -22,6 +24,9 @@ public class PlayerController : MonoBehaviour
 
 
     private float _fireCooldown;
+
+    private bool _isWaitingForZoomOut;
+    private float _zoomOutCooldown;
 
     private Transform _transform;
     private Rigidbody2D _rigidbody;
@@ -38,6 +43,8 @@ public class PlayerController : MonoBehaviour
 	    _turret = GetComponent<Turret>();
 	    _zoomValue = _camera.orthographicSize;
 	    _soundbank = GetComponent<Soundbank>();
+
+	    _isWaitingForZoomOut = false;
 	}
 	
 	// Update is called once per frame
@@ -86,8 +93,10 @@ public class PlayerController : MonoBehaviour
                 _turret.Fire();
                 _soundbank.Play();
                 _fireCooldown = _fireSpeed;
-                _zoomValue -= _zoomModifier;
+                _zoomValue -= _zoomInSpeed;
                 _zoomValue = Mathf.Max(_zoomValue, _zoomMax);
+                _isWaitingForZoomOut = true;
+                _zoomOutCooldown = _timeToZoomOut;
             }
             else
             {
@@ -96,8 +105,23 @@ public class PlayerController : MonoBehaviour
         }
         else
         {
-            _zoomValue += _zoomModifier;
-            _zoomValue = Mathf.Min(_zoomValue, _zoomMin);
+            if (_isWaitingForZoomOut)
+            {
+
+                if (_zoomOutCooldown <= 0.0f)
+                {
+                    _isWaitingForZoomOut = false;
+                }
+                else
+                {
+                    _zoomOutCooldown -= Time.deltaTime;
+                }
+            }
+            else
+            {
+                _zoomValue += _zoomOutSpeed;
+                _zoomValue = Mathf.Min(_zoomValue, _zoomMin);
+            }
         }
 
         _camera.orthographicSize = Mathf.SmoothDamp(_camera.orthographicSize, _zoomValue, ref _dtZoom, _smoothTime, _maxSpeed);
