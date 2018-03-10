f820a5a3b3ec12da6512470cb3941ee26195b224
ksHDO <hiddendeathorb@gmail.com>
2018-03-10 13:01:48 -0700

    Add multiplayer high score submit (submits host only)

--- TunnelVision/Assets/Scripts/Player/PlayerScore.cs
+++ TunnelVision/Assets/Scripts/Player/PlayerScore.cs
@@ -9,13 +9,17 @@ public class PlayerScore : MonoBehaviour
 {
     [SerializeField] private float _score;
     [SerializeField] private FloatEvent OnNewHighScore;
+    [SerializeField] private string _highScorePlayerPref = "HighScore";
+    [SerializeField] private string _eventKey = "UPDATE_SCORE";
+    [SerializeField] private string _eventAttribute = "SCORE";
     public float Score { get { return _score; } set { _score = value; } }
+    public MultiplayerGameManager MultiplayerManager;
 
     public float HighScore
     {
         get
         {
-            float score = PlayerPrefs.GetFloat("HighScore", 50);
+            float score = PlayerPrefs.GetFloat(_highScorePlayerPref, 50);
             return score;
         }
     }
@@ -30,27 +34,30 @@ public class PlayerScore : MonoBehaviour
 
     public void UpdateHighScore()
     {
-        new LogEventRequest()
-            .SetEventKey("UPDATE_SCORE")
-            .SetEventAttribute("SCORE", (int)_score)
-            .Send((response) =>
-            {
-                if (!response.HasErrors)
+        if (!MultiplayerManager || MultiplayerManager.IsHost)
+        {
+            new LogEventRequest()
+                .SetEventKey(_eventKey)
+                .SetEventAttribute(_eventAttribute, (int)_score)
+                .Send((response) =>
                 {
-                    Debug.Log("Successfully submitted score");
-                    
-                }
-                else
-                {
-                    Debug.LogWarning("Could not submit score");
-                    Debug.LogWarning(response.Errors.JSON);
-                }
-            });
+                    if (!response.HasErrors)
+                    {
+                        Debug.Log("Successfully submitted score");
 
-        float score = PlayerPrefs.GetFloat("HighScore", 50);
+                    }
+                    else
+                    {
+                        Debug.LogWarning("Could not submit score");
+                        Debug.LogWarning(response.Errors.JSON);
+                    }
+                });
+        }
+
+        float score = PlayerPrefs.GetFloat(_highScorePlayerPref, 50);
         if (_score > score)
         {
-            PlayerPrefs.SetFloat("HighScore", _score);
+            PlayerPrefs.SetFloat(_highScorePlayerPref, _score);
             OnNewHighScore.Invoke(_score);
         }
     }
